3b73b09e10cb03ca68cf0a51b1b4675ce3645ca4
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-09-01 15:04:36 +0200

    fix issue with elements taking up to much space

--- frontend/src/layouts/DesktopLayout.tsx
+++ frontend/src/layouts/DesktopLayout.tsx
@@ -3,11 +3,11 @@ import PageHeader from "../components/PageHeader";
 
 export default function DesktopLayout() {
     return (
-        <>
+        <div className="h-screen flex flex-col">
             <PageHeader />
-            <div className="flex ">
+            <div className="flex flex-1 min-h-0 w-screen">
                 <Outlet />
             </div>
-        </>
+        </div>
     )
 }
