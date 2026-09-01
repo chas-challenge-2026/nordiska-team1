c9b329c5c1c0402193df88f1ee61da8ef46921f4
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-09-01 11:22:16 +0200

    add routes for landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
 import PageHeader from './components/PageHeader'
+import LandingPage from './pages/LandingPage'
+import { Routes, Route } from 'react-router'
 
 function App() {
-  return (
-    <>
-      <PageHeader></PageHeader>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/welcome" element={<LandingPage />} />
+                <Route path="/inactive" element={<LandingPage inactive />} />
+            </Routes>
+        </>
+    )
 }
 
 export default App
