5359d472e04ff428e97d12a78bacf2613a86df0b
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-09-03 09:25:48 +0200

    fix spelling

--- frontend/src/components/PageNavigation.tsx
+++ frontend/src/components/PageNavigation.tsx
@@ -5,7 +5,7 @@ export default function PageNavigation() {
         <nav className="flex items-center justify-center gap-10 pt-10">
             <PageLink title="Översikt" route="/" />
             <PageLink title="Bankkonton" route="/accounts" />
-            <PageLink title="Tansaktioner" route="/transactions" />
+            <PageLink title="Transaktioner" route="/transactions" />
             <PageLink title="Överföringar" route="/transfer" />
         </nav>
     )
