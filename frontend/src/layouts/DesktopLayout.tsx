c83477c29c52189f7bc5b604beb121cea8d5e842
Dennis.C <dennis.cederqvist@chasacademy.se>
2026-09-03 13:12:45 +0200

    created a footer, links points to nordiska.se currently

--- frontend/src/layouts/DesktopLayout.tsx
+++ frontend/src/layouts/DesktopLayout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router";
 import PageHeader from "../components/PageHeader";
 import PageNavigation from "../components/PageNavigation";
+import PageFooter from "../components/PageFooter";
 
 export default function DesktopLayout() {
-    return (
-        <div className="h-screen flex flex-col">
-            <PageHeader />
-            <PageNavigation />
-            <div className="flex flex-1 min-h-0 w-screen">
-                <Outlet />
-            </div>
-        </div>
-    )
+  return (
+    <div className="h-screen flex flex-col">
+      <PageHeader />
+      <PageNavigation />
+      <div className="flex flex-1 min-h-0 w-screen">
+        <Outlet />
+      </div>
+      <PageFooter />
+    </div>
+  );
 }
