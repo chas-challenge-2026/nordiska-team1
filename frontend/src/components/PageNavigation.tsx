46f7f5e189cfa1112c10be4f70fae072d69eeb47
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-09-07 11:50:12 +0200

    implementing lang-lib in Page-navigation

--- frontend/src/components/PageNavigation.tsx
+++ frontend/src/components/PageNavigation.tsx
@@ -1,12 +1,15 @@
 import PageLink from "./PageLink";
+import { useTranslation } from "react-i18next";
 
 export default function PageNavigation() {
+    const {t} = useTranslation();
+
     return (
         <nav className="flex items-center justify-center gap-10 pt-10">
-            <PageLink title="Översikt" route="/" />
-            <PageLink title="Bankkonton" route="/accounts" />
-            <PageLink title="Transaktioner" route="/transactions" />
-            <PageLink title="Överföringar" route="/transfer" />
+            <PageLink title={t("page-navigation.overview")} route="/" />
+            <PageLink title={t("page-navigation.accounts")} route="/accounts" />
+            <PageLink title={t("page-navigation.transactions")} route="/transactions" />
+            <PageLink title={t("page-navigation.transfers")} route="/transfer" />
         </nav>
     )
 }
