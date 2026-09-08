0bfb43af60547774f38d53d14c4f7c9f3b7c51ef
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-09-08 12:24:14 +0200

    create simple login form to test endpoint

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -3,10 +3,10 @@ import { Navigate, Outlet } from "react-router";
 export default function ProtectedRoute() {
     // ÄNDRA NÄR INLOGG ÄR PÅ PLATS
     // useAuth context? Eller useUser context?
-    const isAuthenticated = true;
+    const isAuthenticated = sessionStorage.getItem("token");
 
     if (!isAuthenticated) {
-        return <Navigate to="/login" replace />;
+        return <Navigate to="/welcome" replace />;
     }
 
     return <Outlet />
