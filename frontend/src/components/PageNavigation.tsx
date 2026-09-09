89be211be18a2ba4109e1a67ebb3655b76691147
Dennis.C <dennis.cederqvist@chasacademy.se>
2026-09-09 13:00:22 +0200

    added pb-3 to nav in PageNavigation as the orange 'highligt' sat underneath, making text underneath look wonky

--- frontend/src/components/PageNavigation.tsx
+++ frontend/src/components/PageNavigation.tsx
@@ -5,7 +5,7 @@ export default function PageNavigation() {
     const {t} = useTranslation();
 
     return (
-        <nav className="flex items-center justify-center gap-10 pt-10">
+        <nav className="flex items-center justify-center gap-10 pt-10 pb-3">
             <PageLink title={t("page-navigation.overview")} route="/" />
             <PageLink title={t("page-navigation.accounts")} route="/accounts" />
             <PageLink title={t("page-navigation.transactions")} route="/transactions" />
