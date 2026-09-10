889ecf0a77b74625cc2be2f93d16b45e88229875
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-09-10 13:45:01 +0200

    cant remember what i did here

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -4,7 +4,7 @@ import ProtectedRoute from "./ProtectedRoute";
 // Open
 import LoginPage from "../pages/LoginPage";
 import LandingPage from "../pages/LandingPage";
-import CollapsiblePlayground from "../pages/CollapsiblePlayground";
+// import CollapsiblePlayground from "../pages/CollapsiblePlayground";
 import PageNotFound from "../pages/PageNotFound";
 
 //Protected
@@ -16,19 +16,25 @@ export default function AppRoutes() {
 
     return (
         <Routes>
-            <Route path="/login" element={<LoginPage />} />
+            {/* <Route path="/dev/collapsible" element={<CollapsiblePlayground />} /> */}
             <Route path="/welcome" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/inactive" element={<LandingPage inactive />} />
-            <Route path="/dev/collapsible" element={<CollapsiblePlayground />} />
             <Route path="*" element={<PageNotFound />} />
 
             {/* PROTECTED ROUTES HÄR */}
             <Route element={<ProtectedRoute/>}>
+
                 <Route path="/" element={<DesktopLayout />}>
-                    <Route index element={<OverviewPage />} />
-                    <Route path="/transactions" element={<TransactionsPage />} />
+                    <Route element={<ProtectedRoute/>} >
+
+                        <Route index element={<OverviewPage />} />
+                        <Route path="/transactions" element={<TransactionsPage />} />
+
+                    </Route>
                 </Route>
             </Route>
+
         </Routes>
-    )
-}
+    );
+};
