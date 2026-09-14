c015f51b8e762f0a32bcef09284d20396fc55b9d
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-09-14 14:18:28 +0200

    put filter and account select in modal on mobile

--- frontend/src/pages/TransactionsPage.tsx
+++ frontend/src/pages/TransactionsPage.tsx
@@ -4,6 +4,7 @@ import TransactionTable from '../components/transactions/TransactionsTable';
 import AccountSelector from '../components/transactions/AccountSelector';
 import TransactionFilter from '../components/transactions/TransactionsFilter';
 import type { TransactionFilters } from '../components/transactions/TransactionsFilter';
+import Modal from '../components/modals/Modal';
 
 const initialFilters: TransactionFilters = {
     search: '',
@@ -18,21 +19,51 @@ export default function TransactionsPage() {
         accounts.map(a => a.id)
     );
     const [filters, setFilters] = useState<TransactionFilters>(initialFilters);
+    const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
 
     return (
-        <div className='flex gap-8 w-screen max-h-screen p-6 font-montserrat'>
-            <TransactionFilter onChange={setFilters} onReset={() => setFilters(initialFilters)} />
+        <div className='flex flex-col md:flex-row gap-8 w-screen max-h-screen p-6 font-montserrat'>
+            <div className='hidden md:block'>
+                <TransactionFilter onChange={setFilters} onReset={() => setFilters(initialFilters)} />
+            </div>
+
+            <button
+                type='button'
+                onClick={() => setIsFilterModalOpen(true)}
+                className='md:hidden self-start rounded-lg border border-gray-300 px-4 py-2 text-sm font-medium'
+            >
+                Filters & Accounts
+            </button>
 
             <TransactionTable
                 selectedAccountIds={selectedAccountIds}
                 filters={filters}
             />
 
-            <AccountSelector
-                accounts={accounts}
-                selectedIds={selectedAccountIds}
-                onChange={setSelectedAccountIds}
-            />
+            <div className='hidden md:block'>
+                <AccountSelector
+                    accounts={accounts}
+                    selectedIds={selectedAccountIds}
+                    onChange={setSelectedAccountIds}
+                />
+            </div>
+
+            {isFilterModalOpen && (
+                <Modal
+                    title='Filters & Accounts'
+                    onClose={() => setIsFilterModalOpen(false)}
+                    widthClassName='w-[90vw]'
+                >
+                    <div className='flex flex-col gap-4 overflow-y-auto p-6'>
+                        <TransactionFilter onChange={setFilters} onReset={() => setFilters(initialFilters)} />
+                        <AccountSelector
+                            accounts={accounts}
+                            selectedIds={selectedAccountIds}
+                            onChange={setSelectedAccountIds}
+                        />
+                    </div>
+                </Modal>
+            )}
         </div>
     );
 }
