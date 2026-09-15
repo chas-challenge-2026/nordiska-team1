4794bd294f463f740c008cfbbe1e76424593bd4f
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-09-15 12:15:03 +0200

    disabled ESLint on line 18

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -14,6 +14,7 @@ export default function LoginPage() {
     const [orderRef, setOrderRef] = useState("");
 
     const {mutate: login, isPending:loginPending, isError: loginIsError, error: loginError } = useLogin();
+    // eslint-disable-next-line
     const {mutate: bankIdInit, isPending: bankIdInitPending, isError:bankIdInitIsError, error: bankIdInitError } = useBankIdInitate();
 
     const bankIdCollect = useBankIdCollect(orderRef);
