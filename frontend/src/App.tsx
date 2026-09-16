30d824d22d6a661f6155270813be3d333e68e857
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-09-16 16:14:16 +0200

    passing route-props to PageHeader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,29 @@ import './App.css'
 import AppRoutes from './routes/AppRoutes'
 import { useUserStore } from './store/userStore'
 import { useSessionCheck } from './hooks/useSessionCheck'
-import PageHeader from './components/PageHeader'
+import PageHeader from './components/header/PageHeader'
+import { useLocation } from 'react-router'
 
 export default function App() {
+    const location = useLocation();
+
     const isCheckingSession = useUserStore((state) => state.isCheckingSession);
     useSessionCheck();
     if (isCheckingSession) return <div>loading...</div>
+
+    const UNPROTECTED_HEADER = [
+        "/welcome",
+        "/inactive",
+        "/login",
+        "/logout",
+        "/logged-out",
+    ]
+
+    const protectedHeader = !UNPROTECTED_HEADER.includes(location.pathname);
     
     return (
         <>
-            <PageHeader />
+            <PageHeader protectedHeader={protectedHeader}/>
             <AppRoutes/>
         </>
     )
