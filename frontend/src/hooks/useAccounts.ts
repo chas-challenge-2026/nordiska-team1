c00d3c4d0fc6b3ea9c978492e2594b51a364fbbe
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-09-16 23:24:19 +0200

    create api services and hooks for accounts

--- frontend/src/hooks/useAccounts.ts
+++ frontend/src/hooks/useAccounts.ts
@@ -1,13 +1,50 @@
-import { useQuery } from "@tanstack/react-query";
-import { getAccounts } from "../services/accountsService";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { type AccountTypes, getAccount, getAccounts, createAccount, closeAccount } from "../services/accountsService";
+
+interface CreateAccountInput {
+    accountName: string | null;
+    accountType: AccountTypes;
+    initialDeposit?: number;
+    interestRate?: number;
+}
 
 export const accountKey = {
     all: ["accounts"] as const,
+    detail: (id: number) => [...accountKey.all, id] as const,
 };
 
-export function useAccounts() {
+export function useGetAccounts() {
     return useQuery({
         queryKey: accountKey.all,
         queryFn: getAccounts,
     });
+}
+
+export function useGetAccount(id: number) {
+    return useQuery({
+        queryKey: accountKey.detail(id),
+        queryFn: () => getAccount(id),
+        enabled: !!id,
+    })
+}
+
+export function useCreateAccount() {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (input: CreateAccountInput) => createAccount(input.accountName, input.accountType, input.initialDeposit, input.interestRate),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: accountKey.all })
+        },
+    })
+}
+
+export function useCloseAccount() {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (id: number) => closeAccount(id),
+        onSuccess: (_, id) => {
+            queryClient.invalidateQueries({queryKey: accountKey.all})
+            queryClient.invalidateQueries({queryKey: accountKey.detail(id)})
+        }
+    })
 }
