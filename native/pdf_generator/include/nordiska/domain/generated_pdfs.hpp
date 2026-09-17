eb83f5729273ca6130ff6c79ded4de5a1a975610
jimmyjordanSWE <169525180+jimmyKodar@users.noreply.github.com>
2026-09-17 21:06:18 +0200

    feat(domain): add SignatureSlot and sha256_hash metadata to PdfDocument

--- native/pdf_generator/include/nordiska/domain/generated_pdfs.hpp
+++ native/pdf_generator/include/nordiska/domain/generated_pdfs.hpp
@@ -1,14 +1,24 @@
 #pragma once
 
+#include <array>
+#include <cstddef>
 #include <cstdint>
 #include <string>
 #include <vector>
 
 namespace nordiska {
 
+struct SignatureSlot {
+    size_t offset{0};
+    size_t max_length{0};
+    bool is_signed{false};
+};
+
 struct PdfDocument {
     std::string document_id;
     std::vector<uint8_t> pdf_bytes;
+    std::array<uint8_t, 32> sha256_hash{};
+    SignatureSlot signature_slot{};
 };
 
 struct GeneratedPdfs {
@@ -17,3 +27,4 @@ struct GeneratedPdfs {
 };
 
 } // namespace nordiska
+
