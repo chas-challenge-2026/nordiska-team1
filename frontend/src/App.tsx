1a89f17cdc2e3db375feb5d6688c2429fcc74236
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-09-21 12:03:39 +0200

    using inactivity warning

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,17 @@ import { useUserStore } from './store/userStore'
 import { useSessionCheck } from './hooks/useSessionCheck'
 import PageHeader from './components/header/PageHeader'
 import { useLocation } from 'react-router'
+import { useInactivityTimer } from './hooks/useInactivityTimer'
+import InactivityWarning from './components/InactivityWarning'
 
 export default function App() {
     const location = useLocation();
 
     const isCheckingSession = useUserStore((state) => state.isCheckingSession);
     useSessionCheck();
+
+    const {showWarning, remainingSeconds, stayLoggedIn, logoutNow} = useInactivityTimer();
+
     if (isCheckingSession) return <div>loading...</div>
 
     const UNPROTECTED_HEADER = [
@@ -26,6 +31,12 @@ export default function App() {
         <>
             <PageHeader protectedHeader={protectedHeader}/>
             <AppRoutes/>
+            {showWarning && (
+                <InactivityWarning 
+                    remainingSeconds={remainingSeconds}
+                    onStayLoggedIn={stayLoggedIn}
+                    onLogout={logoutNow}/>
+            )}
         </>
     )
 }
