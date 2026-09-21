765768fcfcb7935b6df3ee1308e3fa3d5f9f9a58
Dennis.C <dennis.cederqvist@chasacademy.se>
2026-09-21 15:05:24 +0200

    created a Error 500 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import AppRoutes from './routes/AppRoutes'
 import { useUserStore } from './store/userStore'
 import { useSessionCheck } from './hooks/useSessionCheck'
 import PageHeader from './components/header/PageHeader'
+import ErrorBoundary from './components/ErrorBoundary'
 import { useLocation } from 'react-router'
 
 export default function App() {
@@ -25,7 +26,9 @@ export default function App() {
     return (
         <>
             <PageHeader protectedHeader={protectedHeader}/>
-            <AppRoutes/>
+            <ErrorBoundary key={location.pathname}>
+                <AppRoutes/>
+            </ErrorBoundary>
         </>
     )
 }
